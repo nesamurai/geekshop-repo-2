21be1fa22b54f499309d10aab4f0f513aa87ea0f
Anton Petrov <nesamurai@yandex.ru>
2021-10-19 23:33:16 +0300

    created templates, views, urls for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    title = 'GeekShop - Авторизация'
+    return render(request, 'users/login.html', {'title': title})
+
+def registration(request):
+    title = 'GeekShop - Регистрация'
+    return render(request, 'users/registration.html', {'title': title})
