b73114832359eac2e9175144078c718271910885
Anton Petrov <nesamurai@yandex.ru>
2021-10-24 19:01:05 +0300

    created profile with ability to edit it

--- users/views.py
+++ users/views.py
@@ -20,18 +20,31 @@ def login(request):
     title = 'GeekShop - Авторизация'
     return render(request, 'users/login.html', {'title': title, 'form': form})
 
+
 def registration(request):
     if request.method == 'POST':
         form = forms.UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
             messages.add_message(request, messages.SUCCESS, "Вы успешно зарегистрировались!")
-            # messages.success(request, 'Вы успешно зарегистрировались!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = forms.UserRegistrationForm()
     title = 'GeekShop - Регистрация'
     return render(request, 'users/registration.html', {'title': title, 'form': form})
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = forms.UserProfileForm(instance=request.user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = forms.UserProfileForm(instance=request.user)
+    title = 'GeekShop - Профиль'
+    return render(request, 'users/profile.html', {'title': title, 'form': form})
+
 
 def logout(request):
     auth.logout(request)
